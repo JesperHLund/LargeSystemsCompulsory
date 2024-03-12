2414f5dfe91c0ba3dd2e9993891477824301b504
JesperHLund <jesperlund9@hotmail.com>
2024-03-12 21:20:14 +0100

    Feat: Added SubtractionService

---
+++ SubtractionService/index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const axios = require("axios");
+const app = express();
+const cors = require("cors");
+const PORT = 3001;
+
+app.use(cors());
+app.use(express.json());
+
+const subtract = async (numberOne, numberTwo) => {
+    return numberOne - numberTwo;
+}
+
+app.post("/subtract", async (req, res) => {
+    const { numberOne, numberTwo } = req.body;
+    const result = await subtract(numberOne, numberTwo);
+    res.send({ result });
+});
+
+app.listen(PORT, () => {
+    console.log(`Subtraction Service is running on port ${PORT}`);
+});
