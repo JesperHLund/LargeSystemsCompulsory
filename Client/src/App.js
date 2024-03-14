6d9b392562184a24f118a647227140b61e152a0f
Sintry1 <Dominic_smith2012@hotmail.co.uk>
2024-03-14 11:05:48 +0100

    Hopefully fixed missing dependency

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
-function App() {
+export default function App() {
+  const [numberOne, setNumberOne] = useState();
+  const [numberTwo, setNumberTwo] = useState();
+  const [operation, setOperation] = useState();
+  const [result, setResult] = useState("")
+  const host = "http://localhost:3001";
+
+  const handleSubmit = async () => {
+    try{
+      const response = await fetch(`${host}/${operation}`, {
+        method:"POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          numberOne: numberOne,
+          numberTwo: numberTwo
+        }),
+      }).then((response) => response.json())
+      .then((data) => {
+        setResult(data.result);
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input
+        className="Input"
+        type="text"
+        placeholder="First number"
+        onChange={(e) => setNumberOne(e.target.value)}
+      />
+      <select id="operation" onChange={(e) => setOperation(e.target.value)}>
+        <option value="">Select Operation</option>
+        <option value="add">Add</option>
+        <option value="subtract">Subtract</option>
+      </select>
+      <input
+        className="Input"
+        type="text"
+        placeholder="Second Number"
+        onChange={(e) => setNumberTwo(e.target.value)}
+      />
+      <button className="Button" onClick={() => handleSubmit()}>
+        Calculate
+      </button>
     </div>
   );
 }
-
-export default App;
--- databaseservice/index.js
+++ databaseservice/index.js
@@ -2,10 +2,21 @@ const express = require("express");
 const axios = require("axios");
 const app = express();
 const cors = require("cors");
+const mysql = require("mysql");
 const PORT = 3004;
 
 app.use(cors());
 app.use(express.json());
+
+const db = mysql.createConnection({
+    host: "localhost",
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD
+})
+
+app.post("add", async (req, res) => {
+
+});
 
 app.listen(PORT, () => {
     console.log(`Database service is running on port ${PORT}`);
